3373a9e239545bab9ffa8b768d911dd9065278e6
Rayrsn <25773909+Rayrsn@users.noreply.github.com>
2025-10-24 11:35:58 +0300

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/jvc-essentials-showcase/',
+  base: process.env.GITHUB_ACTIONS ? '/jvc-essentials-showcase/' : '/',
   server: {
     host: "::",
     port: 8080,
